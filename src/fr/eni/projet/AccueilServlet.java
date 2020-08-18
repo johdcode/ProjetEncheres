fd35ba2fb2a315418456dfed7ae20016f64e6ba7
johdcode <66391424+johdcode@users.noreply.github.com>
2020-08-18 18:26:19 +0200

    CLEAN

--- src/fr/eni/projet/AccueilServlet.java
+++ src/fr/eni/projet/AccueilServlet.java
@@ -31,7 +31,7 @@ public class AccueilServlet extends HttpServlet {
 		PrintWriter printWritter = response.getWriter();
 		try {
 			Connection c = ConnectionProvider.getConnection();
-			Utilisateur u = new Utilisateur(1, "jooo", "John", "Edouard", "john@mail.com", "0654854596", "Rue Shuman", "54896", "Nantes", "motDePasse", 0.6f, false);
+			Utilisateur u = new Utilisateur("jo", "John", "Edouard", "john@mail.com", "0654854596", "Rue Shuman", "54896", "Nantes", "motDePasse", 0.6f, false);
 			UtilisateurManager.getInstance().insert(u);
 			
 		} catch (SQLException e) {
