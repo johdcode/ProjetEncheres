7d413bc8bd205c9ca33aafb6ed91bfa06736645f
Kim <68189659+kimmaroe@users.noreply.github.com>
2020-08-18 17:51:45 +0200

    jsp connexion et servlet connexion update

--- WebContent/WEB-INF/Connexion.jsp
+++ WebContent/WEB-INF/Connexion.jsp
@@ -8,18 +8,19 @@
 </head>
 <body>
 <h1>ENI-Enchères</h1>
+
 <form method="post">
 <label for="identifiant">Identifiant :</label>
-<input type="text" id="identifiant" name="identifiant">
+<input required type="text" id="identifiant" name="identifiant">
 <label for="motDePasse">Mot de passe :</label>
 <input type="text" id="motDePasse" name="motDePasse">
-<input type="submit" id="connexion" name="connexion" value="Connexion">
+<input required type="submit" id="connexion" name="connexion" value="Connexion">
 <label for="seSouvenirDeMoi">Se souvenir de moi</label>
 <input type="checkbox" id="seSouvenirDeMoi" name="seSouvenirDeMoi" value="seSouvenirDeMoi">
 <a href="/ServletMotDePasseOublie">Mot de passe oublié</a>
 
 </form>
-
+<p><c:if test="${authentification = false}">Le mot de passe ou l'identifiant est incorrect</c:if></p>
 <a href="/WEB-INF/Inscription.jsp">Créer un compte</a>
 </body>
 </html>
---
+++ src/fr/eni/projet/ConnexionServlet.java
@@ -0,0 +1,59 @@
+package fr.eni.projet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Connexion
+ */
+@WebServlet("/ConnexionServlet")
+public class ConnexionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd =this.getServletContext().getRequestDispatcher("/WEB-INF/Connexion.jsp");
+		rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String seSouvenirDeMoi = request.getParameter("seSouvenirDeMoi");
+		String identifiant =request.getParameter("identifiant");
+		String motDePasse =request.getParameter("motDePasse");
+		String messageAuthentification = "Le mot de passe ou l'idenfiant est incorrect";
+		HttpSession session = request.getSession();
+		
+		boolean authentification = false;
+		UtilisateurManager um = um.getInstance();
+		authentification = um.authentification(identifiant, motDePasse);
+				
+		if(authentification=true) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Accueil.jsp"); 
+			System.out.println("connecté");
+		}
+		else {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Connexion.jsp"); 
+			if(seSouvenirDeMoi.contentEquals("seSouvenirDeMoi")) {
+				session.setAttribute("suiviSession", seSouvenirDeMoi);
+				
+			}
+		}
+		
+		
+		
+	}
+
+}
