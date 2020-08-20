62aa8d439555193769903851ea0065545f1bb339
Kim <68189659+kimmaroe@users.noreply.github.com>
2020-08-20 15:56:50 +0200

    Update DAL UtilisateurDAO
    Update delete methods

--- src/fr/eni/projet/test/Testsk.java
+++ src/fr/eni/projet/test/Testsk.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import fr.eni.projet.bll.EnchereManager;
+import fr.eni.projet.bll.UtilisateurManager;
 import fr.eni.projet.bo.Enchere;
 import fr.eni.projet.dal.DALException;
 
@@ -23,24 +24,33 @@ public class Testsk extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		EnchereManager em = EnchereManager.getInstance();
-		Enchere e = new Enchere(new Timestamp(2020-8-20), 12,2,6);
-		em.insert(e);
-		e.setNoArticleVenduEnchere(4);
+
+		UtilisateurManager um = UtilisateurManager.getInstance();
 		try {
-			em.update(e);
-		} catch (DALException e1) {
+			um.delete(4);
+		} catch (DALException e) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			e.printStackTrace();
 		}
+		
+		//		EnchereManager em = EnchereManager.getInstance();
+//		Enchere e = new Enchere(new Timestamp(2020-8-20), 12,2,6);
+//		em.insert(e);
+//		e.setNoArticleVenduEnchere(4);
 //		try {
-//			em.delete(1);
+//			em.update(e);
 //		} catch (DALException e1) {
 //			// TODO Auto-generated catch block
 //			e1.printStackTrace();
 //		}
-		System.out.println(em.selectAll());
-		System.out.println(em.selectById(2));
+////		try {
+////			em.delete(1);
+////		} catch (DALException e1) {
+////			// TODO Auto-generated catch block
+////			e1.printStackTrace();
+////		}
+//		System.out.println(em.selectAll());
+//		System.out.println(em.selectById(2));
 		
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
