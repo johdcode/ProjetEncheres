fb9e481870d074b0c34558d63329988661faeb12
Kim <68189659+kimmaroe@users.noreply.github.com>
2020-08-21 14:05:58 +0200

    UPDATE servelt et JSP nouvelleVente

--- src/fr/eni/projet/ihm/NouvelleVenteServlet.java
+++ src/fr/eni/projet/ihm/NouvelleVenteServlet.java
@@ -1,8 +1,7 @@
 package fr.eni.projet.ihm;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Timestamp;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,9 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import fr.eni.projet.bll.CategorieManager;
-import fr.eni.projet.bo.Categorie;
 import fr.eni.projet.dal.DALException;
 
 /**
@@ -41,7 +38,94 @@ public class NouvelleVenteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//TODO select by categorie
+		//récupération de la saisie utilisateur
+		String article = request.getParameter("article");
+		String description = request.getParameter("description");
+		String categorie = request.getParameter("categorie");
+		String photo = request.getParameter("photo");
+		String prix = request.getParameter("prix");
+		Timestamp debutEnchere = Timestamp.valueOf(request.getParameter("debut-enchere")) ;
+		Timestamp finEnchere = Timestamp.valueOf(request.getParameter("fin-enchere"));
+		String rue = request.getParameter("rue");
+		String cp = request.getParameter("cp");
+		String ville = request.getParameter("ville");
+		
+		//affichage saisie pour vérification
+		System.out.println("article :"+article);
+		System.out.println("article :"+description);
+		System.out.println("article :"+categorie);
+		System.out.println("article :"+photo);
+		System.out.println("article :"+prix);
+		System.out.println("article :"+debutEnchere);
+		System.out.println("article :"+finEnchere);
+		System.out.println("article :"+rue);
+		System.out.println("article :"+cp);
+		System.out.println("article :"+ville);
+		
+		
+		//vérification des erreurs formulaire
+		int erreur = 0;
+		if(article == null || article == "") {
+			erreur++;
+			}
+		
+		if(article != null && article.length() >= 50) {
+			erreur++;
+			}
+		
+		if(description == null || description == "") {
+			erreur++;
+		}
+		if(description != null && description.length() >= 50) {
+			erreur++;
+			}
+		if(categorie == null || categorie == "") {
+			erreur++;
+		}
+		//TODO liste bdd
+		if(categorie != null && categorie.length() >= 50) {
+			erreur++;
+			}
+		//TODO erreur fichier(taille? format?)
+		if(photo == null || photo == "") {
+			erreur++;
+		}
+		if(photo != null && photo.length() >= 50) {
+			erreur++;
+			}
+		if(prix == null || prix == "") {
+			erreur++;
+		}
+		if(prix != null && prix.length() >= 9) {
+			erreur++;
+			}
+		//TODO format date?
+		if(debutEnchere == null ) {
+			erreur++;
+		}
+		
+		//TODO format date?
+		if(finEnchere == null) {
+			erreur++;
+		}
+	
+		if(rue == null || rue == "") {
+			erreur++;
+		}
+		if(rue != null && rue.length() >= 30) {
+			erreur++;
+			}
+		if(cp == null || cp == "") {
+			erreur++;
+		}
+		if(cp != null && cp.length() == 5) {
+			erreur++;
+			}
+		if(erreur == 0) {
+			response.sendRedirect(request.getContextPath() + "/encheres");
+		} else {
+			response.getWriter().print("Il y a eu une erreur dans la recherche.");
+		}
 	}
 
 }
