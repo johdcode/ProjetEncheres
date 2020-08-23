f52150408ae6f085aae4e05f9262d941caf5c393
johdcode <66391424+johdcode@users.noreply.github.com>
2020-08-23 19:23:10 +0200

    Update SessionService.java
    Ajout de commentaires

--- src/fr/eni/projet/ihm/SessionService.java
+++ src/fr/eni/projet/ihm/SessionService.java
@@ -9,6 +9,13 @@ import fr.eni.projet.bo.Utilisateur;
 
 public class SessionService {
 	
+	/**
+	 * Récuperer l'utisateur dont l'id stockée en session correspond si elle existe,
+	 * et le place comme attribut de la requête
+	 * 
+	 * @param request
+	 * @return utilisateur
+	 */
 	public static Utilisateur checkUtilisateurSession(HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		Utilisateur u = null;
@@ -23,11 +30,23 @@ public class SessionService {
 		return u;
 	}
 	
+	/**
+	 * Démarre une session: définie l'id de l'utilisateur dans la session
+	 * 
+	 * @param request
+	 * @param id
+	 */
 	public static void setUtilisateurSessionId(HttpServletRequest request, int id) {
 		HttpSession session = request.getSession();
 		session.setAttribute("utilisateurSessionId", id);
 	}
 	
+	/**
+	 * Recupère l'id stocké en session
+	 * 
+	 * @param request
+	 * @return id
+	 */
 	public static String getUtilisateurSessionId(HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		return (String) session.getAttribute("utilisateurSessionId");
