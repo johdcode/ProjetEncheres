d165682e4f5d3d2f92c723199cdbe956d0d5fee5
johdcode <66391424+johdcode@users.noreply.github.com>
2020-08-24 18:24:07 +0200

    Update ListeEncheresServlet.java

--- src/fr/eni/projet/ihm/ListeEncheresServlet.java
+++ src/fr/eni/projet/ihm/ListeEncheresServlet.java
@@ -14,6 +14,7 @@ import fr.eni.projet.bll.CategorieManager;
 import fr.eni.projet.bll.UtilisateurManager;
 import fr.eni.projet.bo.ArticleVendu;
 import fr.eni.projet.bo.Categorie;
+import fr.eni.projet.bo.Utilisateur;
 import fr.eni.projet.dal.DALException;
 
 /**
